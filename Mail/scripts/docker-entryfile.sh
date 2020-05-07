4ad7721a79de8a138348d9117fc71ee6def64564
maxime <maxime.de.cock98@gmail.com>
2020-05-07 12:46:17 +0200

    dockerfile + all
    ajout de la dockerfile ainsi que des fichier de configuration du serveur mail

---
+++ Mail/scripts/docker-entryfile.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Launch configuration script
+/mail-server/scripts/configure.sh
+
+# Launch services
+service rsyslog start
+service postfix start
+service dovecot start
+
+# Launch logs examination
+tail -f /var/log/dovecot
+© 2020 GitHub, Inc.
